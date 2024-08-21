8417b6983837fe68ef644e1531b9ad0d3c7f1ab7
DKDimik <dimik322@mail.ru>
2024-08-21 11:41:23 +0700

    Initial commit

---
+++ src/Work1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Work1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int num = scan.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("жопа");
+        }
+    }
+}
